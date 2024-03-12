424f4ae178300964b8900fb4a8da39b02b9fbee5
Calvin Pereira <78508059+CalvinJDP@users.noreply.github.com>
2024-03-11 18:09:30 -0700

    Update half_adderSource.v

--- FullAdder/half_adderSource.v
+++ FullAdder/half_adderSource.v
@@ -1,5 +1,22 @@
 `timescale 1ns / 1ps
 
+//////////////////////////////////////////////////////////////////////////////////
+// Author: Calvin Pereira
+// 
+// Create Date: 03/07/2023 02:40:49 PM
+// Module Name: half_adder.v
+// Project Name: FullAdder
+// Tool Versions: Vivado 2019.1
+// Description: Full Adder using a Half Adder
+// 
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
 module half_adder (
     input x,y,
     output sum,carry
